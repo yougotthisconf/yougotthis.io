c0eca8e8d5c079fbb8841161c1ac5ec5b2f494d3
Kevin Lewis <kvn@lws.io>
2022-01-30 20:09:32 +0000

    feat: added branding

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        theme: {
+          main: '#e9194a',
+          light: '#FF6E75',
+          dark: '#C4050F',
+          black: '#331717',
+          white: '#f1f1e7'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
