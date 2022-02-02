80c4c0f682e5f676b30248cf1752f5eb53442dee
Kevin Lewis <kvn@lws.io>
2022-02-02 08:58:34 +0000

    Added type to content component view

--- components/library/Content.vue
+++ components/library/Content.vue
@@ -5,10 +5,10 @@
             <span class="icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path v-if="type == 'video'" fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                    <path v-if="type == 'writing'" fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
+                    <path v-if="type == 'article'" fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
 
                 </svg>
-                <span>{{ length }} mins</span>
+                <span>{{ length }} min {{ type }}</span>
             </span>
             <span v-if="featured" class="featured">Featured</span>
         </div>
@@ -40,7 +40,7 @@ export default {
             required: true
         },
         type: {
-            validator: (value) => ['video', 'writing'].includes(value),
+            validator: (value) => ['video', 'article'].includes(value),
             required: true
         },
         featured: {
--- components/library/ContentList.vue
+++ components/library/ContentList.vue
@@ -3,11 +3,11 @@
         <Content title="It's Not Your Job To Love Your Job" :people="['Keziyah Lewis']" :length="28" type="video" />
         <Content title="Planning and Preparing For Your Next Role" :people="['Rachel Lee Nabors']" :length="35" type="video" />
         <Content title="Using Accessible Language" :people="['Amy Dickens']" :length="43" type="video" />
-        <Content title="Lending Privilege" :people="['Anjuan Simmons']" :length="45" type="writing" />
+        <Content title="Lending Privilege" :people="['Anjuan Simmons']" :length="45" type="article" />
 
         <Content title="Making Yout 1-1s Effective" :people="['Naomi Pentrel']" :length="28" type="video" />
-        <Content title="Perfectionism, Imposter Syndrome, and Anxiety" :people="['Jo Franchetti']" :length="25" type="writing" />
-        <Content title="Negotiating After Your Start Date" :people="['Jeanine Mendez']" :length="23" type="writing" />
+        <Content title="Perfectionism, Imposter Syndrome, and Anxiety" :people="['Jo Franchetti']" :length="25" type="article" />
+        <Content title="Negotiating After Your Start Date" :people="['Jeanine Mendez']" :length="23" type="article" />
         <Content title="Making Your First Days Count" :people="['Nathaniel Okenwa']" :length="26" type="video" />
     </div>
 </template>
