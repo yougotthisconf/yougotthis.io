e67b3069d229d5234a3ac140bb4bd7a9616618fa
Kevin Lewis <kvn@lws.io>
2022-03-17 14:21:57 +0000

    Search on library page

--- pages/library/index.vue
+++ pages/library/index.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
         <div class="wrapper pt-16 mb-16">
-            <div class="bg-green-500 p-4 mb-8">Todo: filters for content type, sorting by alphabetical or latest.</div>
             <h1 class="heading text-center">Library</h1>
+            <div class="filters">
+                <!-- TODO: Add Content Type Filter -->
+                <div class="search">
+                    <input v-model="query" type="text" placeholder="Type to filter" class="text-input">
+                </div>
+            </div>
+
             <ContentList :list="aboveFold" class="mt-8" />
+
+            <div v-if="aboveFold.length === 0" class="no-results">
+                <p>No results. We search titles, descriptions, and speaker names. If there's a topic you would like to cover, let us know!</p>
+            </div>
         </div>
 
         <div class="bg-theme-main text-white">
@@ -21,7 +31,6 @@
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="h-6 w-full bg-theme-white">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" fill="var(--theme-main)"></path>
             </svg>
-
         </div>
 
         <div class="wrapper py-16">
@@ -43,10 +52,51 @@ export default {
             return { ...item, people: profiles }
         })
 
-        const aboveFold = content.slice(0, 12)
-        const belowFold = content.slice(12, content.length)
-
-        return { aboveFold, belowFold, collections }
+        return { content, collections }
+    },
+    data() {
+        return {
+            query: 'adsasd'
+        }
+    },
+    computed: {
+        search() {
+            const l = this.content
+            if(this.query) {
+                const q = this.query.toLowerCase()
+                const results = []
+                results.push(...l.filter(i => i.title.toLowerCase().includes(q)))
+                results.push(...l.filter(i => i.people.map(p => p.name.toLowerCase()).join(', ').includes(q)))
+                results.push(...l.filter(i => i.descriptions.short.toLowerCase().includes(q)))
+                results.push(...l.filter(i => i.descriptions.full.toLowerCase().includes(q)))
+                return [...new Set(results)]
+            }
+            return l
+        },
+        aboveFold() {
+            return this.search.slice(0, 12)
+        },
+        belowFold() {
+            return this.search.slice(12, this.search.length)
+        }
+    },
+    created() {
+        // TODO: Initial query happens from URL
     },
 }
 </script>
+
+<style scoped>
+.search {
+    @apply flex justify-center mt-4;
+    & input {
+        @apply w-full;
+        @apply md:w-64;
+        @apply text-theme-black;
+    }
+}
+.no-results {
+    @apply bg-white text-theme-black rounded-lg transition text-left shadow;
+    @apply mb-4 p-4 w-full;
+}
+</style>
