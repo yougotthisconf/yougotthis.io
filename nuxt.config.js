a3bd69d9b8fe396d8f7f15a55de6b177efcaaca6
Kevin Lewis <kvn@lws.io>
2022-03-19 00:32:17 +0000

    favicons, rss feed

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
-const copyContentImages = require('./utils/copy-content-images.js')
+import config from './config'
+import copyContentImages from './utils/copy-content-images.js'
 copyContentImages()
 
 export default {
@@ -19,10 +20,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#e9194a' },
+      { name: 'msapplication-config', content: '/icons/browserconfig.xml' },
+      { name: 'theme-color', content: '#e9194a' },
+
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'shortcut icon', href: '/icons/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', type: 'image/png' , href: '/icons/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', type: 'image/png' , href: '/icons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/icons/site.webmanifest' },
+      { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#e9194a' },
     ],
     script: [
       { src: 'https://player.vimeo.com/api/player.js' }
@@ -36,11 +46,55 @@ export default {
   modules: [
     '@nuxtjs/axios',
     '@nuxt/content',
+    '@nuxtjs/feed',
+    '@nuxtjs/markdownit'
   ],
   moment: {
 		timezone: true,
 	},
   tailwindcss: {
     cssPath: '~/assets/style.css'
-  }
+  },
+  feed: [
+    {
+      path: '/feed.xml',
+      async create(feed) {
+        feed.options = {
+          title: config.title,
+          description: config.description,
+          link: config.baseURL + '/feed.xml'
+        }
+        const { $content } = require('@nuxt/content') // eslint-disable-line
+        const items = await $content('library', { deep: true }).sortBy('date', 'desc').fetch();
+        items.forEach(item => {
+          const url = `${config.baseURL}/library/${item.slug}`;
+          feed.addItem({
+            title: item.title,
+            id: url,
+            link: url,
+            description: item.descriptions.full,
+            content: item.bodyText,
+          });
+        });
+      },
+      cacheTime: 1000 * 60 * 15,
+      type: 'rss2',
+    },
+  ], // this is the end of the feed settings
+  hooks: {
+    'content:file:beforeInsert': document => {
+      const md = require('markdown-it')() // eslint-disable-line
+      if (document.extension === '.md') {
+        const mdToHtml = md.render(document.text) // eslint-disable-line
+        document.bodyText = mdToHtml
+      }
+    },
+  },
+  markdownit: {
+    preset: 'default',
+    linkify: true,
+    breaks: true,
+    use: ['markdown-it-div', 'markdown-it-attrs'],
+  },
+  // TODO: RSS
 }
