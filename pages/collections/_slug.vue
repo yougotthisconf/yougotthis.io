da9efed358bf5254a9dd11d8c7f971bb983474ce
Kevin Lewis <kvn@lws.io>
2022-05-01 16:57:14 +0100

    Removed date on collection page

--- pages/collections/_slug.vue
+++ pages/collections/_slug.vue
@@ -25,12 +25,6 @@
                     </svg>
                     <span>{{ duration }} total length</span>
                 </div>
-                <div class="stat">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                    </svg>
-                    <span>{{ $moment(collection.date).format('MMM Do YYYY') }}</span>
-                </div>
             </div>
         </div>
         <main class="col-span-2">
