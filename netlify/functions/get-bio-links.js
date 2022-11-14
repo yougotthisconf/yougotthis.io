a908ad12ae26b3cb4a507c3c9f3b61ee97a0cb42
Kevin Lewis <kvn@lws.io>
2022-11-14 16:00:10 +0000

    Fix bio link grabber

--- netlify/functions/get-bio-links.js
+++ netlify/functions/get-bio-links.js
@@ -1,30 +1,23 @@
 const { Client } = require("@notionhq/client")
-const notion = new Client({ auth: process.env.NOTION_INTEGRATION_TOKEN })
+const notion = new Client({ auth: process.env.NOTION_YGT_DB_KEY })
+
+async function getLinks() {
+  const { results } = await notion.databases.query({
+    database_id: process.env.NOTION_YGT_DB_BIOS,
+    filter: { property: 'Visible', checkbox: { equals: true }},
+    sorts: [{ property: 'Date', direction: 'descending' }]
+  })
+  const list = results.map(result => ({
+    label: result.properties.Label.title[0].plain_text,
+    url: result.properties.URL.url
+  }))
+  return list
+}
 
 exports.handler = async event => {
   try {
-    const { results } = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE_ID,
-        filter: {
-            property: 'Visible',
-            checkbox: {
-                equals: true
-            }
-        },
-        sorts: [
-            {
-                property: 'Date',
-                direction: 'descending'
-            }
-        ]
-    })
-    const list = results.map(result => {
-        return {
-            label: result.properties.Label.title[0].plain_text,
-            url: result.properties.URL.url
-        }
-    })
-    return res(list)
+    const links = await getLinks()
+    return res(links)
   } catch (error) {
     return res(error, 500)
   }
