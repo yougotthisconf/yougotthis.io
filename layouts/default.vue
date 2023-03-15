6079d992d9351af303ed494df7bd6afc0785100c
Kevin Lewis <kvn@lws.io>
2023-03-15 21:25:57 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-theme-white min-h-screen flex flex-col">
-        <LiveNow />
+        <!--<LiveNow />-->
         <TheHeader />
         <nuxt class="flex-1 w-full" />
         <TheFooter />
