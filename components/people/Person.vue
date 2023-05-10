bf71ef31c5aa7be233a090e7b26f267e270470f7
Kevin Lewis <kvn@lws.io>
2023-05-10 08:14:15 +0000

    Person

--- components/people/Person.vue
+++ components/people/Person.vue
@@ -1,13 +1,13 @@
 <template>
-    <n-link :to="person.dir" class="box">
+    <n-link :to="`/people/${person.slug}`" class="box">
         <div class="core">
-            <img :src="`${person.dir}/${person.avatar}`" :alt="person.title" class="w-9">
+            <img :src="`${$asset(person.image)}?width=30`" :alt="person.title" class="w-9">
             <p>{{ person.title }}</p>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
         </div>
-        <nuxt-content v-if="bio" :document="person" />
+        <!-- <nuxt-content v-if="bio" :document="person" /> -->
     </n-link>
 </template>
 
@@ -18,10 +18,10 @@ export default {
             type: Object,
             required: true
         },
-        bio: {
-            type: Boolean,
-            default: false
-        }
+        // bio: {
+        //     type: Boolean,
+        //     default: false
+        // }
     }
 }
 </script>
