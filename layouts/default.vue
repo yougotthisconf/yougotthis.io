8da162f6fb9da9c6c1de8806db75d8b657504682
Kevin Lewis <kvn@lws.io>
2023-06-15 09:49:32 +0200

    Added show live now global

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
     <div class="bg-theme-white min-h-screen flex flex-col">
-        <!--<LiveNow />-->
+        <LiveNow v-if="show_live_now" />
         <TheHeader />
         <nuxt class="flex-1 w-full" />
         <TheFooter />
     </div>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            show_live_now: null
+        }
+    },
+    async fetch() {
+        const { data: globals } = await this.$directus.items('globals').readByQuery()
+        this.show_live_now = globals.show_live_now
+    }
+}
+</script>
